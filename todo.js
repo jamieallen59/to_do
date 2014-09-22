bca57d14856683defee73fae846d516cae6f9a90
Jamie Allen <Jamieallen59@gmail.com>
2014-09-22 17:47:57 +0100

    Clear task button now working

--- todo.js
+++ todo.js
@@ -1,12 +1,22 @@
 function TodoCtrl($scope) {
-	$scope.totalTodos = 4;
 
 	$scope.todos = [
 		{text: 'Learn Angular JS', done: false},
 		{text: 'Build an app', done: false}
 	];
 
+	$scope.getTotalTodos = function() {
+		return $scope.todos.length;
+	}
+
 	$scope.addTodo = function() {
 		$scope.todos.push({text: $scope.formTodoText, done: false});
+		$scope.formTodoText = '';
 	}
-}
+
+	$scope.clearCompleted = function() {
+		$scope.todos = _.filter($scope.todos, function(todo) {
+			return !todo.done;
+		});
+	};
+};
