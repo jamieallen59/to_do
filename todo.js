71214d85a128cd4eb56cb63352017cbedd50a5a7
Jamie Allen <Jamieallen59@gmail.com>
2014-09-22 16:04:16 +0100

    dynamically updating list

--- todo.js
+++ todo.js
@@ -5,4 +5,8 @@ function TodoCtrl($scope) {
 		{text: 'Learn Angular JS', done: false},
 		{text: 'Build an app', done: false}
 	];
+
+	$scope.addTodo = function() {
+		$scope.todos.push({text: $scope.formTodoText, done: false});
+	}
 }
